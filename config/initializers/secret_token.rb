12a3692eaa8bee191823e382b2029664418c7d32
Bryan Stearns <bryanstearns@gmail.com>
2013-03-12 10:56:12 -0700

    rails new data-engineering --skip-test-unit --database=sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataEngineering::Application.config.secret_token = 'd739276078b86ad309f0fe38a9ad284683a0f8fa5fff10095ec6c07479813b449dbee4bea86be6247693becb029b83e7b5541e62289335b05f5e4502763f52d2'
